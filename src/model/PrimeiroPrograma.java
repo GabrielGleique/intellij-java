f9298cb2192a0573ab7f674ce849eecfb9d2ab0b
Gabriel <gabrielgleique@hotmail.com>
2022-07-25 22:06:24 -0300

    intellij

--- src/model/PrimeiroPrograma.java
+++ src/model/PrimeiroPrograma.java
@@ -1,7 +1,10 @@
 package model;
 
 
+import jdk.dynalink.linker.support.CompositeGuardingDynamicLinker;
 import model.Gato;
+
+import java.nio.file.DirectoryIteratorException;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
@@ -9,9 +12,45 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+       livro livro1 = new livro("O problema dos 3 corpos" , 300);
+        System.out.println(livro1);
 
         /*int a = 5;
         int b = 3;
         System.out.println("Hell world!" + (a+b));*/
+    }
+}
+
+class livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
